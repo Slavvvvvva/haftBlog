a7b24637f102a099f29c608aa929a0c7c86dd943
slavvvvva <slavvvvva@yandex.ua>
2020-07-16 15:24:21 +0200

    add transition and bigger img on click

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
 'use strict';
 let bigger = document.querySelector('#bigger2');
 let text = document.querySelector('#short');
+let bidimg= document.querySelectorAll('.imgb');
 
-let check = true;
+let check1 = true;
+let check=true;
 
 bigger.addEventListener("click",function(){
     console.log("aga");
-    if (check==true){
+    if (check1==true){
         text.classList.remove("shorter");
-        check = false;
+        check1 = false;
     } else{
         text.classList.add("shorter");
-        check = true;
+        check1= true;
     }
     
 });
+console.log(bidimg);
 
+bidimg.forEach(function(i){
+i.addEventListener("click",function(){
+    console.log("agaimg");
+    if (check==true){
+        i.classList.add("img2activ");
+        check = false;
+    } else{
+        i.classList.remove("img2activ");
+        check= true;
+    }
+    
+});
+});
 
