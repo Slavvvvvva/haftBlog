1a5dc1eed9e6423eedd9a481bcae55849e58c141
slavvvvva <slavvvvva@yandex.ua>
2020-09-21 23:13:26 +0200

    slider bagining

--- js/galary_script.js
+++ js/galary_script.js
@@ -9,35 +9,6 @@ let check1 = true;
 let check = true;
  
 
-// Увеличения изображения
-
-bidimg.forEach(function(i){
-i.addEventListener("click",function(){
-    console.log("agaimg");
-    if (check==true){
-        i.classList.add("img2activ");
-        check = false; 
-
-        //  Ограничение максимальной высоты изображения при изменения розмера окна
-
-        let Img2Activ = document.querySelector('.img2activ');
-        let WindiwHeight = document.documentElement.clientHeight;
-        
-        Img2Activ.style.maxHeight=`${WindiwHeight-50}px`;
-        window.addEventListener('resize', function(){
-            let WindiwHeight = document.documentElement.clientHeight; 
-            console.log("resized");
-            Img2Activ.style.maxHeight=`${WindiwHeight-50}px`;
-        });
-
-    } else{
-        i.classList.remove("img2activ");
-        check= true;
-    }
-    
-});
-});
-
 //  Кнопка появления меню
 
 hamBtn.addEventListener("click",function(){
@@ -51,5 +22,55 @@ hamBtn.addEventListener("click",function(){
     }
 });
 
- 
+// Slider
 
+   //Увеличения всех изображений при клике на любое
+
+let littleSlider = document.querySelectorAll(".little_slider");
+
+littleSlider.forEach(function(i){
+    i.addEventListener("click",function(){
+        console.log("agaimg");
+        if (check==true){
+            littleSlider.forEach(function(i){
+            i.classList.add("img2activ");
+            check = false; 
+            });
+        
+    
+            //  Ограничение максимальной высоты изображения при изменения розмера окна
+    
+            let Img2Activ = document.querySelector('.img2activ');
+            let WindiwHeight = document.documentElement.clientHeight;
+            
+            Img2Activ.style.maxHeight=`${WindiwHeight-50}px`;
+            window.addEventListener('resize', function(){
+                let WindiwHeight = document.documentElement.clientHeight; 
+                console.log("resized");
+                Img2Activ.style.maxHeight=`${WindiwHeight-50}px`;
+            });
+    
+        } else{
+            i.classList.remove("img2activ");
+            check= true;
+        }
+        
+    });
+});
+
+// Сам слайдер
+let slideIndex = 1;
+
+function showSlids(n){
+    if (n > littleSlider) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = littleSlider.length;
+    }
+    littleSlider.forEach(i => i.style.display = "none");
+    littleSlider[slideIndex-1].style.display = "block";
+}
+function plusSlides(n) {
+    showSlids(slideIndex +=n);
+}
